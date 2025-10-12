49d9ebc7a58b952c9299342ba7cbe26c8119e694
Luca Younes <lucaguyyounes@gmail.com>
2025-10-11 22:50:55 -0700

    move custom types from main

---
+++ custom_types.h
@@ -0,0 +1,53 @@
+#ifndef CUSTOM_TYPES_H
+#define CUSTOM_TYPES_H
+#include <fstream>
+#include <unordered_map>
+
+#include "dbcppp/CApi.h"
+#include "dbcppp/Network.h"
+
+#include <iostream>
+#include <cstring>
+#include <cerrno>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include "arrow/api.h"
+#include "arrow/io/api.h"
+#include "arrow/ipc/api.h"
+#include "parquet/arrow/writer.h"
+#include "parquet/api/writer.h"
+#include "parquet/stream_writer.h"
+#include "arrow/io/file.h"
+
+#include <chrono>
+
+
+using canid_t = uint32_t;
+
+// keeps track of indecies and types of signals.. used to put signals in the correct row in parquet by index
+struct SignalTypeOrderTracker
+{
+    // The name of the CAN signal (e.g., "Engine_Speed")
+    std::string signal_name; 
+    
+    // Arrow style type of the signal (will either be float, or int... but maybe we'll extend to treat bool as bool and not int in the future)
+    parquet::Type::type arrow_type; 
+};
+
+// Stores monostate (for null column, or double, or 32/64/128 signed int, or float/double, or bool) to closely match parquet types
+using DataTypeOrVoid = std::variant<std::monostate, double, int32_t, __int128_t, float, bool>;
+
+
+#endif
