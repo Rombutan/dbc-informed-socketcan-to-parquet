579f4ba0d30fc77f4d0114879389781c7d1b477e
Luca Younes <lucaguyyounes@gmail.com>
2025-10-20 13:59:23 -0700

    follow syntactic argument conventions

--- README.md
+++ README.md
@@ -24,34 +24,34 @@ canplayer -i -v vcan0=can0 -I candump.log
 
 # Usage
 ```
-./decoder file.dbc [-of output.parquet] [-if vcan0] [-socket|-file|-stdin] [-cache 10] [-forward-fill] [-live-decode Time_ms]
+./decoder file.dbc [--of output.parquet] [--if vcan0] [--socket|--file|--stdin] [--cache 10] [--forward-fill] [--live-decode Time_ms]
 ```
 You must always provide a dbc file and it must be the first argument. The order of the others might matter, idk.
 
 There are three input options:
-- `-file` will read from the file specified by the `-if <>` argument. This file must be created in the candump format where each line is: `(<epoch in seconds>) <interface> <ID>#<Content>`. This is the default output of `candump -l`
-- `-socket` will read from the socketcan socket specified by the `-if <>` argument.
-- `-stdin` will read from stdin, expecting each line to be in the `candump -l` format. In this mode, `-if` is ignored and optional.
+- `--file` will read from the file specified by the `--if <>` argument. This file must be created in the candump format where each line is: `(<epoch in seconds>) <interface> <ID>#<Content>`. This is the default output of `candump -l`
+- `--socket` will read from the socketcan socket specified by the `--if <>` argument.
+- `--stdin` will read from stdin, expecting each line to be in the `candump -l` format. In this mode, `--if` is ignored and optional.
 
 If you want to cache some period of messages into a single database row, use the `-cache` argument followed by a float or int with unit ms. The timestamp of each output row will be that of the first message which is cached into that row.
 
-The `-forward-fill` option will not clear the cache after writting a row, so once a value is set by the recieve of a message with it's corresponding signal, it will not be set back to null, and will only reset if another message is recieved with that signal.
+The `--forward-fill` option will not clear the cache after writting a row, so once a value is set by the recieve of a message with it's corresponding signal, it will not be set back to null, and will only reset if another message is recieved with that signal.
 
-If you want to print signal values to the terminal as the program runs, use the `[-live-decode <Signal Name>]` option. You must also pass a signal name, which must be in the DBC. You can enable as many live outputs as you want by passing `-live-decode` seveal times. 
+If you want to print signal values to the terminal as the program runs, use the `[--live-decode <Signal Name>]` option. You must also pass a signal name, which must be in the DBC. You can enable as many live outputs as you want by passing `--live-decode` seveal times. 
 
 For Example:
 ```
-./decoder fs.dbc -if can0 -of sep13data_run3.parquet -socket
+./decoder fs.dbc --if can0 --of sep13data_run3.parquet -socket
 ```
 
 If you wish to convert a candump file directly, use something similar to this example:
 ```
-./decoder fs.dbc -if candump-sep13data_run3.log -of sep13data_run3.parquet -file
+./decoder fs.dbc --if candump-sep13data_run3.log -of sep13data_run3.parquet -file
 ```
 
-You can also record on another device using netcat, or anything else of your chosing (including candump in stdout mode for bettter(?) timestamping) by using the `-stdin` argument
+You can also record on another device using netcat, or anything else of your chosing (including candump in stdout mode for bettter(?) timestamping) by using the `--stdin` argument
 ```
-nc -l 9000 | ./decodere fs.dbc -of test.parquet -cache 50 -stdin
+nc -l 9000 | ./decodere fs.dbc --of test.parquet --cache 50 --stdin
 ```
 
 # Weird Shit
--- arguments.h
+++ arguments.h
@@ -30,12 +30,12 @@ struct CommandLineArugments {
 CommandLineArugments parse_cli_arguments(int argc, char* argv[]){
     CommandLineArugments args_out;
     if(argc < 1){
-        std::cout << "you must provide at least a dbc file name... \n ./decoder file.dbc [-of output.parquet] [-if vcan0] [-socket|-file] [-cache 10] \n \"if\" is used for the interface name in socket mode and the file name in file mode \n";
+        std::cout << "you must provide at least a dbc file name... \n Example: \n ./decoder file.dbc [--of output.parquet] [--if vcan0] [--socket|--file] [--cache 10] \n \"if\" is used for the interface name in socket mode and the file name in file mode \n";
     }
 
     int arg = 2;
     while (arg < argc){
-        if(std::strcmp(argv[arg], "-of") == 0){
+        if(std::strcmp(argv[arg], "--of") == 0){
             if (arg + 1 >= argc) {
                 std::cerr << "Error: Missing filename for 'of' option; test.parquet will be used\n";
             }
@@ -43,34 +43,34 @@ CommandLineArugments parse_cli_arguments(int argc, char* argv[]){
             args_out.parquet_filename=argv[arg+1];
             arg++;
 
-        } else if(std::strcmp(argv[arg], "-if") == 0){
+        } else if(std::strcmp(argv[arg], "--if") == 0){
             if (arg + 1 >= argc) {
                 std::cerr << "Error: Missing filename for 'if' option; File name will be vcan0\n";
             }
             std::cout << "Got input file / can interface=" << argv[arg+1] << "\n";
             args_out.can_interface=argv[arg+1];
             arg++;
-        } else if(std::strcmp(argv[arg], "-socket") == 0){
+        } else if(std::strcmp(argv[arg], "--socket") == 0){
             std::cout << "Using SocketCan for input\n";
             args_out.input = SOCKETCAN;
-        } else if(std::strcmp(argv[arg], "-file") == 0){
+        } else if(std::strcmp(argv[arg], "--file") == 0){
             std::cout << "Using file for input\n";
             args_out.input = CANDUMP;
-        } else if(std::strcmp(argv[arg], "-stdin") == 0){
+        } else if(std::strcmp(argv[arg], "--stdin") == 0){
             std::cout << "Using stdin for input\n";
             args_out.input = STDIN;
-        } else if(std::strcmp(argv[arg], "-cache") == 0)  {
+        } else if(std::strcmp(argv[arg], "--cache") == 0)  {
             if (arg + 1 >= argc) {
                 std::cerr << "Error: Missing caching period in ms; No caching active.\n";
             }
             std::cout << "Got caching period=" << argv[arg+1] << "ms\n";
             args_out.cache_ms = std::stod(argv[arg+1]);
             arg++;
-        } else if(std::strcmp(argv[arg], "-forward-fill") == 0)  {
+        } else if(std::strcmp(argv[arg], "--forward-fill") == 0)  {
             std::cout << "Using forward fill\n";
             args_out.forward_fill = true;
             arg++;
-        } else if(std::strcmp(argv[arg], "-live-decode") == 0)  {
+        } else if(std::strcmp(argv[arg], "--live-decode") == 0)  {
             if (arg + 1 >= argc) {
                 std::cerr << "Error: Missing Signal to live decode.\n";
             }
@@ -78,7 +78,7 @@ CommandLineArugments parse_cli_arguments(int argc, char* argv[]){
             args_out.live_decode_signals.push_back(argv[arg+1]);
             arg++;
         } else {
-            std::cout << "Incorrect argument " << argv[argc] << ". Example: \n ./decoder file.dbc [-of output.parquet] [-if vcan0] [-socket|-file] [-cache 10] \n \"if\" is used for the interface name in socket mode and the file name in file mode \n";
+            std::cout << "Incorrect argument " << argv[argc] << ". Example: \n ./decoder file.dbc [--of output.parquet] [--if vcan0] [--socket|--file] [--cache 10] \n \"if\" is used for the interface name in socket mode and the file name in file mode \n";
         }
 
         arg++;
